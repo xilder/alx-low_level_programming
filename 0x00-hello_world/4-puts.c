475bed0522ab2b647f60c0a131da9d55aa24f46d
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-08 07:14:19 -0700

    4-put.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - outputs "Programming is like building a multilingual puzzle"
+ * It returns 0 when completed
+ */
+int main()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return(0);
+}
