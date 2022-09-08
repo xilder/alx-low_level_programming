6ca0fb5907e88eae381cec2ca6a2127131386603
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-08 08:01:02 -0700

    6.size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - puts out "with proper grammar, but the outcome is a piece of art"
+ * main - puts out sizes of types 
  * Return: ends function
  */
 int main(void)
 {
-	prinf("Size of a char: %d byte(s)\n", sizeof(char));
-	prinf("Size of a int: %d byte(s)\n", sizeof(int));
-	prinf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	prinf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	prinf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
