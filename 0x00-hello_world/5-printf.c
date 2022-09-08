8d340746db9922c8de7389c80f1ca1263eafd918
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-08 07:38:01 -0700

    5-print.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - puts out "with proper grammar, but the outcome is a piece of art"
+ * Return: ends function
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
