edfc124e8cc8ffc567b75c17794b45c2145e3ed6
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-09 14:33:14 -0700

    6-print_numberziutchar('\n');

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - outputs 0 to 9
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
