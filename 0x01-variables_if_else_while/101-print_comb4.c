b971e17d52814db9a064dcd1e96e1e5b158f2970
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-09 16:31:52 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - returns a range of numbers
+ * Return: always false
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a <= 55; a++)
+	{
+		for (b = 49; b <= 56; b++)
+		{
+			for (c = 50; c <= 57; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56 || c !=57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
