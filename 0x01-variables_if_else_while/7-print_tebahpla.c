5e05c71ea63cdae4bbac575153b624318c66a8fd
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-09 15:22:13 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - alphabet in reverse
+ * Return: always false
+ */
+
+int main(void)
+{
+	char ch;
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
