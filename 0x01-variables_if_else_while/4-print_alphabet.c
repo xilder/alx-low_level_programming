89033d5539079b64a493c6ea119310ee1ae45736
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-09 14:01:19 -0700

    4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets excpet q and e
+ * Return: always 0
+ */
+int main (void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'e' && a != 'q')
+		{
+			putchar(a);
+		}
+	}
+	return (0);
+}
