b5424e7cb0b5952765d701896f49cb20926f8353
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-09 14:19:34 -0700

    5-print_numbers5-print_numbers5-print_numbers5-print_numbers5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - outputs 0 to 9
+ * Return - always 0
+ */
+int main(void)
+{
+	int a;
+
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
