784f2b84d6e670ebdeb070c83c55a260ca2a98b4
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-13 02:35:12 -0700

    2-print_alphabet_x10

---
+++ 0x02-functons_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints _putchar then a new line
+ * Return: always false
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
