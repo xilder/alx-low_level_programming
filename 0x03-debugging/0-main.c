7cf4cb52a093b54645f3cb012ae8648bcfb4cd4d
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-14 15:15:38 -0700

    0-main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests functions that if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = -58;
+	positive_or_negatve(i);
+	return (0);
+}
