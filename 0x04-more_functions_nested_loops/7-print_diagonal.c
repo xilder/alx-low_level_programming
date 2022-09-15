8bec3205820d97a71c4575f33063ee6bb5565714
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-15 16:35:33 -0700

    7-print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints numbers from 0 to 9
+ * @n: number of underscore
+ * Return: returns 0 or 1
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(' ');
+	}
+	_putchar('\\')
+	_putchar('\n');
+}
