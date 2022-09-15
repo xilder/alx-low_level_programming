bebd864805e4971ce749307c24c292a0eac176eb
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-15 16:15:36 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14
+ * Return: returns 0 or 1
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+		if (i >= 10)
+		{
+			_putchar('1');
+		}
+		_putchar((i % 10) + 48);
+	}
+	_putchar('\n');
+}
