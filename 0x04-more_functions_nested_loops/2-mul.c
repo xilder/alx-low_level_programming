1b13e60fe5a027c15560d97b38372b8f5ff337c6
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-15 15:24:53 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - multiplies the digit input
+ * @a: the number checked
+ * @b: the number checked
+ * Return: returns 0 or 1
+ */
+
+int mul(int a, int b)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
