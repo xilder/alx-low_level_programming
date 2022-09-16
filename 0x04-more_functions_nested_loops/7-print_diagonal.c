5f9212bb2ff25702d0377a99f2e002473d5c1559
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-15 17:39:48 -0700

    diagonal with '#' will be ignored, and an empty message aborts the commit.

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,10 +10,26 @@ void print_diagonal(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		_putchar(' ');
+		_putchar('\n');
 	}
-	_putchar('\\');
-	_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if ( j == 1)
+				{
+					_putchar('\\');
+				}
+				else if (j < i)
+				{
+					_putchar(' ');
+				}
+			}
+		}
+		putchar('\n');
+	}
 }
