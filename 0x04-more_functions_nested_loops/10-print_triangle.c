97d1a58f4d06936b8a5dde69f1218146eb55f95e
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-15 19:27:29 -0700

    print triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - prints numbers from 0 to 9
+ * @size: number of triangles
+ * Return: returns 0 or 1
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
