e5d089494d31bd1c913d4d2876737441a06acfff
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-19 13:51:00 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+/**
+ * _puts - prints a string to the output
+ * @str - string to be put
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
