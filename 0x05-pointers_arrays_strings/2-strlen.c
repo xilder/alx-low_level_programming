9933a26620e51a92b67197c134cfa5e88c96e4ff
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-19 13:20:59 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+/**
+ * _strlen - returns number of character in a string
+ * @s: string to be passed
+ * Return: return length
+ */
+
+int _strlen(char *s)
+{
+	int i = sizeof(*s);
+
+	return (i / 4);
+}
