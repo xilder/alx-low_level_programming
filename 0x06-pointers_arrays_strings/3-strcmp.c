5c5c12a82c8ba1d32dd61ad903d0c3a9336f7591
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-22 05:52:58 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - to compare strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: returns an integer depending on the the function
+ */
+
+char *_strcmp(char *s1, char *s2)
+{
+	int i = 0, n = 0, j = 15;
+
+	while (s1[n] != '\0')
+	{
+		n++;
+	}
+	while (s2[i] != '\0')
+	{
+		i++;
+	}
+
+	if (n > i)
+		return (j);
+	else if (n < 1)
+		return (-j);
+	else
+		return (0);
+}
