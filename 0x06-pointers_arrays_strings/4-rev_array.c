5d8e5ac8bc28f3fae3a739a5d12687a43e3be5c0
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-22 06:46:41 -0700

    rev array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/** reverse_array - reverses the order of the array
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+    int i;
+
+    i = n - 1;
+    while (i > 0)
+    {
+        if (i != n - 1)
+        {
+            printf(", ");
+        }
+        printf("%d", a[i]);
+        i--;
+    }
+    printf("\n");
+}
