8d9e29744b161470103a6f26ec2bbda51f0b1373
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-22 07:36:37 -0700

    to upper case# Please enter the commit message for your changes. Lines starting

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts any character ina string to uppercase
+ * @s: string to be passed
+ * Return: a string with all uppercase characters
+ */
+
+char *string_toupper(char *s)
+{
+	int i, j = 0;
+
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < j; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}	
+	}
+	return (s);
+}
