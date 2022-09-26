756bbc677b055215e9c9cecdce72cab3a06ce059
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-26 13:36:29 -0700

    print diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,7 +14,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		x += a[(size * i) + i];
-		y += a[(size * (i + 1) - (i + 1)];
+		y += a[(size * (i + 1)) - (i + 1)];
 	}
 	printf("%d, %d\n", x, y);
 }
