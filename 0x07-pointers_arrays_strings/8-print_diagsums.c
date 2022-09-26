e64346caf44ac8da12223d6e8f448f1673da4821
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-26 13:35:06 -0700

    print_diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of the diagonals of the square
+ * @a: perfect array
+ * @size: size of the root of the square
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	unsigned int x = 0, y = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		x += a[(size * i) + i];
+		y += a[(size * (i + 1) - (i + 1)];
+	}
+	printf("%d, %d\n", x, y);
+}
