7b6884a0797d9a747091fdf49d23e8bccde38d80
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-26 11:40:01 -0700

    istrpbrk with '#' will be ignored, and an empty message aborts the commit.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * _strpbrk - locates a char from a string
+ * @s: string to be searched
+ * @accept: string searched
+ * Return: return address for result
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[j] == accept[i])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return ('\0');
+}
