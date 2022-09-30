7b5d207d50fc17210a9bcb612e897a7233a7d83f
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-09-30 07:20:20 -0700

    add 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n" argv[1] * argv[2]);
+	return (0);
+}
