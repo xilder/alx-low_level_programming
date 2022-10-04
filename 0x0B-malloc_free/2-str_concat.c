fa0efd4c27d3bf14e3071dba66a8f5ddc4f2504f
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-10-04 14:39:15 -0700

    modify 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,6 +20,8 @@ char *str_concat(char *s1, char *s2)
 	while (s2[len2])
 		len2++;
 	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
+		 return (NULL);
 	if (s1)
 	{
 		for (i = 0; s1[i] != '\0'; i++)
