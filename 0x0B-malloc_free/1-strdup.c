3498bd611df6b6e4d84d31d96310fa7a02acadad
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-10-04 14:11:47 -0700

    add 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - copies the string given as parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to the copied string (Success)
+ */
+cha r*_strdup(char *str)
+{
+	char *dup;
+	unsigned int i = 0; len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+	dup = malloc(sizeof(char) * (len + 1));
+
+	if (dup == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	return (dup);
+}
