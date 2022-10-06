d770d6aa37710a6986ccc4c09c1dff171e09be5b
Abel Fagbemi <olabowaleayobami@gmail.com>
2022-10-06 05:34:50 -0700

    add 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_calloc - allocates space for nmemb element
+ * each of size size
+ * @nmemb: number of elements to be accommodated
+ * @size: size of each member
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *s;
+	unsigned int i = 0;
+	
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	s = malloc(size * nmemb);
+	if (!s)
+		return (NULL);
+
+	while(i < size)
+	{
+		s[i] = 0;
+	}
+	return (s);
+}
