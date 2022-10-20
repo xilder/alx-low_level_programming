2d309dc5304d18aaec4825abad33597e0ab49342
xilder <olabowaleayobami@gmail.com>
2022-10-20 17:41:02 +0000

    modify 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,13 @@
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
+
 	while (h)
 	{
 		if (!h -> str)
 			printf("[0] (nil)\n");
 		else
-			printf("%s", h->next);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		s++;
 	}
