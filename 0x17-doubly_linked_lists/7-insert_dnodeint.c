e8af2b7debaaf58365d99cbb31bb2a7862b65c0b
xilder <olabowaleayobami@gmail.com>
2023-08-20 14:13:34 +0000

    mods 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,8 +19,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	do {
 		if (head->next == NULL)
-		{	new = add_dnodeint(head, n);
+		{	new = add_dnodeint_end(h, n);
 			return (new);
+		}
 		if (i == idx)
 		{
 			new = malloc(sizeof(dlistint_t));
