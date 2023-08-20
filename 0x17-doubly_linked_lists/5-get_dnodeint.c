cbd0cf32d5827033d858bedbb96bcadab176f73d
xilder <olabowaleayobami@gmail.com>
2023-08-20 02:16:16 +0000

    adds 5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - returns the address of the node at index
+ * @head: pointer to the head of the list
+ * @index: index of the node to be returned
+ * Return: the address of the node or NULL if the function fails
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *current;
+
+	if (head == NULL)
+		return (NULL);
+	do {
+		current = head;
+		head = head->next;
+		i++;
+	} while (i <= index);
+
+	return (current);
+}
