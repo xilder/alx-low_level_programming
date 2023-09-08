1118f896f8e7bda739400901c6bed8a5bf74db53
xilder <olabowaleayobami@gmail.com>
2023-09-08 21:31:44 +0000

    mods 0-hash_tables_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,13 +11,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	unsigned long int i;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
+
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	
 	if (new_table->array == NULL)
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+		hash_table_t->array[i] = NULL;
+
 	return (new_table);
 }
