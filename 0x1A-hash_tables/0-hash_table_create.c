0aa98e0f85df431689a8f97d47640728b0153314
xilder <olabowaleayobami@gmail.com>
2023-09-09 09:46:40 +0000

    mod files

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,7 +5,7 @@
  * @size: size of the hash table
  *
  * Return: a pointer to the newly created hash table or
- * null if it fails
+ * 	null if it fails
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
@@ -13,13 +13,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 	unsigned long int i;
 
+
 	new_table = malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
 		return (NULL);
 
 	new_table->size = size;
-	new_table->array = malloc(size * sizeof(hash_node_t *));
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (new_table->array == NULL)
 		return (NULL);
