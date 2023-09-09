b1a6fdd53888e83a7ddb6ce0a66b9fbb04be6cdf
xilder <olabowaleayobami@gmail.com>
2023-09-09 03:53:49 +0000

    adds 6-hash_table_delete.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -22,11 +22,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->array = malloc(size * sizeof(hash_node_t *));
 
 	if (new_table->array == NULL)
-	{
-		free(new_table);
 		return (NULL);
-	}
+
 	for (i = 0; i < size; i++)
 		new_table->array[i] = NULL;
+
 	return (new_table);
 }
---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_table_t *table = ht;
+	hash_node_t *tmp, *head;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			head = ht->array[i];
+			while (head != NULL)
+			{
+				tmp = head->next;
+				free(head->key);
+				free(head->value);
+				free(head);
+				head = tmp;
+			}
+		}
+	}
+	free(table->array);
+	free(table);
+}
